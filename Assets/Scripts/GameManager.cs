43bccec6bccc60fd3a0479af2d60360824c3c0d6
dcalitri <dcalitri@student.fitchburgstate.edu>
2023-02-15 22:32:58 -0500

    set the menus in Unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public bool isGameActive;
+    public bool isGameActive = false;
+    private PlayerController playerController;
+    public TextMeshProUGUI distanceText;
+    public TextMeshProUGUI sizeText;
+    public TextMeshProUGUI mainMenu;
+    public TextMeshProUGUI gameOverScreen;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
+        playerController.isGameOver = false;
+    }
+
+    public void GameOver()
+    {
+        if (playerController.isGameOver == true)
+        {
+            isGameActive = false;
+            gameOverScreen.enabled = true;
+        }
     }
 }
